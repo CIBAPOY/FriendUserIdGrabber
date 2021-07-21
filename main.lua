93ce2db19ddf9ab33c40416fb2ff94f533f6ee50
CIBAPOY <87725488+CIBAPOY@users.noreply.github.com>
2021-07-21 01:18:47 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,22 +6,36 @@ local Player = game.Players.LocalPlayer
 game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.All, false)
 
 local Players = game:GetService("Players")
+
 local Friends = script.Parent
 Friends.Loading.Visible = true
 
 local USERNAME = game.Players.LocalPlayer.Name
 
+
+
+
+
+
+
+
+
+
 local function iterPageItems(pages)
 	return coroutine.wrap(function()
+
 		local pagenum = 1
 		while true do
 			for _, item in ipairs(pages:GetCurrentPage()) do
+					
 				coroutine.yield(item, pagenum)
 			end
 			if pages.IsFinished then
+					
 				break
 			end
 			pages:AdvanceToNextPageAsync()
+		
 			pagenum = pagenum + 1
 		end
 	end)
@@ -32,9 +46,15 @@ local userId = Players:GetUserIdFromNameAsync(USERNAME)
 local friendPages = Players:GetFriendsAsync(userId)
 
 local usernames = {}
+
 for item, pageNo in iterPageItems(friendPages) do
+	
 	usernames[item.Username] = item.Id
+	
 end
+
+
+
 
 
 
@@ -49,15 +69,20 @@ for v,i in pairs(usernames) do
 	local b = ButtonClone:Clone()
 	b.Parent = Friends.TextButtons
 	b.Name = v
+	
 	b.Text = v
 	
 	local added = Instance.new("BoolValue", b)
 	added.Name = v
+	
 	added.Value = false
+	
 	b.MouseButton1Click:Connect(function()
+			
 		added.Value = not added.Value
 		
 		if added.Value == true then
+				
 			b.BackgroundColor3 = Color3.new(0, 0.666667, 0)
 		else
 			b.BackgroundColor3 = Color3.new(255, 255, 255)
@@ -69,14 +94,34 @@ end
 
 Friends.Loading.Visible = false
 
+
+
+
+
+
+
+
+
+
 local loco = game.Players.LocalPlayer.Name
 
 local userids = ""
 local hi = "Start"
 
 local remove = Friends["Toggle Remove"].RemoveValue
+
 local keep = Friends["Toggle Keep"].KeepValue
+
 game.Players[loco].PlayerGui.Main.Friends.UserIds.Text = ''
+
+
+
+
+
+
+
+
+
 
 Friends.Generate.MouseButton1Click:Connect(function()
 
